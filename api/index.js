759655dff8ab8ecc915bd510c0f3c93924db691c
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-05-19 16:07:15 +0500

    safayi nisk imaan

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
 const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
 
 app.use(cors({
-  origin: '*'
+	origin: '*'
 }));
 
 app.use(bodyParser.json());
@@ -32,52 +32,56 @@ const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 async function run() {
-  try {
-    // const database = client.db('sample_mflix');
-    // const movies = database.collection('movies');
+	try {
+		// const database = client.db('sample_mflix');
+		// const movies = database.collection('movies');
 
-    // // Query for a movie that has the title 'Back to the Future'
-    // const query = { title: 'Back to the Future' };
-    // const movie = await movies.findOne(query);
+		// // Query for a movie that has the title 'Back to the Future'
+		// const query = { title: 'Back to the Future' };
+		// const movie = await movies.findOne(query);
 
-    console.log("idhar");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+		console.log("idhar");
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
 }
 run().catch(console.dir);
 console.log(process.env.GOOGLE_DRIVE_CLIENT_ID);
 console.log(process.env.GOOGLE_DRIVE_CLIENT_SECRET);
 console.log(process.env.GOOGLE_DRIVE_REDIRECT_URI);
 console.log(process.env.GOOGLE_DRIVE_REFRESH_TOKEN);
-(async () => {
-  const googleDriveService = new GoogleDriveService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
-  const finalPath = path.resolve(__dirname, '../Frame34.svg');
-  const folderName = 'Picture';
+async function uploadFileToDrive() {
+	const googleDriveService = new GoogleDriveService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
+	const finalPath = path.resolve(__dirname, '../Frame34.svg');
+	const folderName = 'Picture';
 
-  if (!fs.existsSync(finalPath)) {
-    throw new Error('File not found!');
-  }
-  
-  let folder = await googleDriveService.searchFolder(folderName).catch((error) => {
-    console.error(error);
-    return null;
-  });
-  
-  if (!folder) {
-    folder = await googleDriveService.createFolder(folderName);
-  }
-  
-  await googleDriveService.saveFile('SpaceX', finalPath, 'image/jpg', folder.id).catch((error) => {
-    console.error(error);
-  });
+	if (!fs.existsSync(finalPath)) {
+		throw new Error('File not found!');
+	}
 
-  console.info('File uploaded successfully!');
+	let folder = await googleDriveService.searchFolder(folderName).catch((error) => {
+		console.error(error);
+		return null;
+	});
 
-  // Delete the file on the server
-  fs.unlinkSync(finalPath);
-})();
-  app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-  });
+	if (!folder) {
+		folder = await googleDriveService.createFolder(folderName);
+	}
+
+	await googleDriveService.saveFile('SpaceX', finalPath, 'image/jpg', folder.id).catch((error) => {
+		console.error(error);
+	});
+
+	console.info('File uploaded successfully!');
+
+	// Delete the file on the server
+	fs.unlinkSync(finalPath);
+
+}
+
+
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
