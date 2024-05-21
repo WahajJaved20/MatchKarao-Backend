c1c3013ac5ab795979285d16c67be683672a9657
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-05-21 15:23:46 +0500

    registerQueryLocked

--- api/index.js
+++ api/index.js
@@ -20,10 +20,11 @@ const driveClientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET || '';
 const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
 const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
 
-app.use(cors({
-	origin: 'https://match-karao.vercel.app' 
-  }));
+// app.use(cors({
+// 	origin: 'https://match-karao.vercel.app'
+// }));
 
+app.use(cors())
 app.use(bodyParser.json());
 
 const { MongoClient } = require("mongodb");
@@ -33,30 +34,10 @@ const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri);
 
-async function run() {
-	try {
-		// const database = client.db('sample_mflix');
-		// const movies = database.collection('movies');
-
-		// // Query for a movie that has the title 'Back to the Future'
-		// const query = { title: 'Back to the Future' };
-		// const movie = await movies.findOne(query);
-
-		console.log("idhar");
-	} finally {
-		// Ensures that the client will close when you finish/error
-		// await client.close();
-	}
-}
-run().catch(console.dir);
-console.log(process.env.GOOGLE_DRIVE_CLIENT_ID);
-console.log(process.env.GOOGLE_DRIVE_CLIENT_SECRET);
-console.log(process.env.GOOGLE_DRIVE_REDIRECT_URI);
-console.log(process.env.GOOGLE_DRIVE_REFRESH_TOKEN);
-async function uploadFileToDrive() {
+async function uploadFileToDrive(folderNamse, fileName) {
 	const googleDriveService = new GoogleDriveService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
 	const finalPath = path.resolve(__dirname, '../Frame34.svg');
-	const folderName = 'Picture';
+	const folderName = folderNamse;
 
 	if (!fs.existsSync(finalPath)) {
 		throw new Error('File not found!');
@@ -81,79 +62,91 @@ async function uploadFileToDrive() {
 	fs.unlinkSync(finalPath);
 
 }
+
+
 app.post("/login", async (req, res) => {
 	try {
 		console.log("trynna log in")
-	  const {teamName, password} = req.body;
-	  const db = client.db("MatchKarao")
-	  const collection = db.collection('Credentials');
-	  const md5Hash = crypto.createHash('md5').update(password).digest('hex');
-	  collection.find({teamName: teamName}).toArray().then((result) => {
-		if (!result || result.length == 0) {
-		  res.status(200).json({ message: "Invalid username or password. Please try again.", type:"Failed" });
-		  return;
-		}
-		  if(result[0].teamName == teamName && result[0].password == md5Hash){
-			const token = jwt.sign({ userId: teamName }, secretKey, { expiresIn: '1h' });
-			console.log("Returning success status")
-			res.status(200).json({ message: token, type:"Success" }); 
-			console.log("mubarak")
-		  }else{
-			res.status(200).json({message: "Invalid username or password. Please try again.", type:"Failed"})
-		  }
-	  }).catch((error) => {
+		const { teamName, password } = req.body;
+		const db = client.db("MatchKarao")
+		const collection = db.collection('Credentials');
+		const md5Hash = crypto.createHash('md5').update(password).digest('hex');
+		collection.find({ teamName: teamName }).toArray().then((result) => {
+			if (!result || result.length == 0) {
+				res.status(200).json({ message: "Invalid username or password. Please try again.", type: "Failed" });
+				return;
+			}
+			if (result[0].teamName == teamName && result[0].password == md5Hash) {
+				const token = jwt.sign({ userId: teamName }, secretKey, { expiresIn: '1h' });
+				console.log("Returning success status")
+				res.status(200).json({ message: token, type: "Success" });
+				console.log("mubarak")
+			} else {
+				res.status(200).json({ message: "Invalid username or password. Please try again.", type: "Failed" })
+			}
+		}).catch((error) => {
+			console.error(error);
+			res.status(500).json({ message: 'Internal Server Error' });
+		});
+	} catch (error) {
 		console.error(error);
 		res.status(500).json({ message: 'Internal Server Error' });
-	  });
-	} catch (error) {
-	  console.error(error);
-	  res.status(500).json({ message: 'Internal Server Error' });
 	}
-	});
-	app.post('/verifyJWT', async (req, res) => {
-		try{
-		  const {jwtToken} = req.body;
-		  let decodedToken;
-		  if(!jwtToken){
-			res.status(200).json({ message: 'Invalid JWT Token', type:"Failed"});
+});
+app.post('/verifyJWT', async (req, res) => {
+	try {
+		const { jwtToken } = req.body;
+		let decodedToken;
+		if (!jwtToken) {
+			res.status(200).json({ message: 'Invalid JWT Token', type: "Failed" });
 			return;
-		  }
-		  try{
-			decodedToken = jwt.verify(jwtToken, secretKey);
-		  }catch(error){
-			console.error(error);
-			res.status(200).json({ message: 'Invalid JWT Token', type:"Failed"});
-			return;
-		  }
-		  res.status(200).json({ message: decodedToken , type:"Success"});
-		}catch(error){
-		  console.error(error);
-		  res.status(500).json({ message: 'Internal Server Error' });
 		}
-	  });
-	  
-app.post('/register', (req, res) => {
-    try {
+		try {
+			decodedToken = jwt.verify(jwtToken, secretKey);
+		} catch (error) {
+			console.error(error);
+			res.status(200).json({ message: 'Invalid JWT Token', type: "Failed" });
+			return;
+		}
+		res.status(200).json({ message: decodedToken, type: "Success" });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
+});
+
+app.post('/register', async (req, res) => {
+	try {
 		const db = client.db("MatchKarao")
-        const {teamName, password} = req.body;
-        const collection = db.collection('Credentials');
-        const md5Hash = crypto.createHash('md5').update(password).digest('hex');
-        collection.find({teamName: teamName}).toArray().then((result) => {
-        if(result && result.length > 0){
-            res.status(200).json({ message: "User already exists", type:"Failed" });
-        }else{
-            collection.insertOne({teamName: teamName, password: md5Hash}).then(()=>{
-                    res.status(200).json({ message: "User registered successfully", type:"Success" });
-            })            
-        }
-        }).catch((error) => {
-            console.error(error);
-            res.status(500).json({ message: 'Internal Server Error' });
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+		const formData = req.body;
+		const { teamName, password, confirmPassword, location, image } = formData;
+		const collection = db.collection('Credentials');
+		const md5Hash = crypto.createHash('md5').update(password).digest('hex');
+		collection.find({ teamName: teamName }).toArray().then(async (result) => {
+			if (result && result.length > 0) {
+				res.status(200).json({ message: "User already exists", type: "Failed" });
+			} else {
+				const result = await collection.insertOne({
+					teamName: teamName,
+					password: md5Hash,
+					confirmPassword: confirmPassword,
+					location: location,
+					image: image
+				}).catch((error) => {
+					console.error(error)
+					res.status(500).json({ message: "Failed to Add user", type: "Failure" })
+				})
+				console.log(result.insertedId)
+				res.status(200).json({ message: "User registered successfully", type: "Success", docID: result.insertedId });
+			}
+		}).catch((error) => {
+			console.error(error);
+			res.status(500).json({ message: 'Internal Server Error' });
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
 })
 
 
